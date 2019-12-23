59f2010dc57a3f92cf730a0c96a6b5956109b79d
Rmnzm <romka5562@gmail.com>
2019-12-23 18:33:44 +0300

    Some changes

--- JS/my.js
+++ JS/my.js
@@ -1,9 +1,19 @@
 
 $(document).ready(function(){
+  var easyThing = 0;
+  var normalThing = 0;
+  var hardThing = 0;
+  var easyStatus = $("span#easy");
+  var normalStatus = $("span#normal");
+  var hardStatus = $("span#hard");
+  easyStatus.text(easyThing);
+  normalStatus.text(normalThing);
+  hardStatus.text(hardThing);
   $("#create").on("click", function(){
     var thing = $("#thing");
     var time = $("#time");
-    var hard = $("#hard option:selected");
+    var hard = $("select#hard option:selected");
+
     if(thing.val().trim().length == 0){
       alert("Введите вашу задачу");
     }else{
@@ -14,6 +24,21 @@ $(document).ready(function(){
         list.append("<li class='"+hard.val()+"'>"+thing.val()+" - <b> "+time.val()+" минут</b></li>");
         thing.val("");
         time.val("");
+        switch(hard.val()){
+          case "easy":
+            easyThing++;
+            easyStatus.text(easyThing);
+            break;
+          case "normal":
+            normalThing++;
+            normalStatus.text(normalThing);
+            break;
+          case "hard":
+          hardThing++;
+          hardStatus.text(hardThing);
+          break;
+        }
+
     }
     }
   });
